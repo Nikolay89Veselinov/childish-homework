243dcc720a55fe3e53449bd6ee770f074f52fa30
nikolay89veselinov <nikolay89veselinov>
2022-08-28 11:56:20 +0300

    create get_area and get_perimeter methods in triangle and square models

--- polygons/models.py
+++ polygons/models.py
@@ -1,10 +1,17 @@
+import math
 from django.db import models
+
+
+def distance2D(xA,yA,xB,yB):
+    return math.sqrt((xA-xB)*(xA-xB)+(yA-yB)*(yA-yB))
 
 
 class Point(models.Model):
     x = models.FloatField()
     y = models.FloatField()
 
+    def __str__(self):
+        return f'x={self.x}, y={self.y}'
 
 class Polygon(models.Model):
     vertices = models.ManyToManyField(Point)
@@ -16,6 +23,31 @@ class Polygon(models.Model):
 class Triangle(Polygon):
     pass
 
+    def get_area(self):
+        param_1, param_2, param_3 = self.vertices.all()
+        x1, y1 = param_1.x, param_1.y
+        x2, y2 = param_2.x, param_2.y
+        x3, y3 = param_3.x, param_3.y
+        area = (x1*y2 + x2*y3 + x3*y1) - (y1*x2 + y2*x3 + y3*x1)
+        area = abs(area * 0.5)
+        return(area)
+
+    def get_perimeter(self):
+        param_1, param_2, param_3 = self.vertices.all()
+        perimeter = distance2D(param_1.x,param_1.y,param_2.x,param_2.y)+distance2D(param_1.x,param_1.y,param_3.x, param_3.y)+distance2D(param_3.x,param_3.y,param_2.x,param_2.y)
+        return perimeter
+
 
 class Square(Polygon):
     pass
+
+    def get_area(self):
+        param_1, param_2, param_3, param_4 = self.vertices.all()
+        area = abs(param_1.x - param_1.y)
+        return(abs(area * area))
+
+
+    def get_perimeter(self):
+        param_1, param_2, param_3, param_4 = self.vertices.all()
+        area = abs(param_1.x - param_1.y)
+        return(abs(area * 4))
