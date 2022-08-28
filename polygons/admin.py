e560f51d20073962df0a84b17e5f46d73d417dc3
nikolay89veselinov <nikolay89veselinov>
2022-08-28 11:52:02 +0300

    register models in admin panel

--- polygons/admin.py
+++ polygons/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Point, Triangle, Square
+
+
+@admin.register(Point)
+class PointAdmin(admin.ModelAdmin):
+    list_display = ('id', 'x', 'y')
+
+
+@admin.register(Triangle)
+class TriangleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Square)
+class SquareAdmin(admin.ModelAdmin):
+    pass
