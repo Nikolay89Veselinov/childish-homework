866a213f2efc8529b138f48a701767536f7c79ce
nikolay89veselinov <nikolay89veselinov>
2022-08-30 16:46:58 +0300

    Create Serializers

--- api/polygons/serializers.py
+++ api/polygons/serializers.py
@@ -1,15 +1,41 @@
 from rest_framework import serializers
 
-from polygons.models import Triangle, Square
+from polygons.models import Triangle, Square, Polygon, Point
 
 
-class TriangleSerializer(serializers.HyperlinkedModelSerializer):
+class PointSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Point
+        fields = ('x', 'y')
+
+
+class TriangleSerializer(serializers.ModelSerializer):
+    vertices = PointSerializer(many=True)
+
     class Meta:
         model = Triangle
-        fields = '__all__'
+        fields = ('id', 'vertices')
 
 
 class SquareSerializer(serializers.ModelSerializer):
+    square_id = serializers.IntegerField()
+    point_x = serializers.FloatField()
+    point_y = serializers.FloatField()
+
     class Meta:
         model = Square
-        fields = '__all__'
+        fields = ('square_id', 'point_x', 'point_y')
+
+    def validate(self, value):
+        square_id = self.initial_data['square_id']
+        point_x = float(self.initial_data['point_x'])
+        point_y = float(self.initial_data['point_y'])
+        square = Square.objects.get(id=square_id)
+        x = square.vertices.values()[0]['x']
+        y = square.vertices.values()[0]['y']
+
+        if point_x >= x and point_y >= x and point_x <= y and point_y <= y:
+            return True
+        else:
+            return False
